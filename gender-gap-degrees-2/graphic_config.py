b48880b841443650040693d80badad2d9d99048f
Christopher Groskopf <chrisgroskopf@gmail.com>
2017-05-28 16:16:02 -0500

    gender-gap-degrees-2: initial import.

---
+++ gender-gap-degrees-2/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '13oFZD2onGjkE01zkWUFHy6UxScCboCU8dlV0Qxo1-Po'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
