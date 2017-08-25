c9433b983273f6b6d48f34571d5b4213e57d46ff
Christopher Groskopf <chrisgroskopf@gmail.com>
2017-08-25 10:37:40 -0500

    housing-wealth: initial import.

---
+++ housing-wealth/js/graphic.js
@@ -0,0 +1,290 @@
+// Global vars
+var pymChild = null;
+var isMobile = false;
+
+/*
+ * Initialize the graphic.
+ */
+var onWindowLoaded = function() {
+    if (Modernizr.svg) {
+        formatData();
+
+        pymChild = new pym.Child({
+            renderCallback: render
+        });
+    } else {
+        pymChild = new pym.Child({});
+    }
+
+    pymChild.onMessage('on-screen', function(bucket) {
+        ANALYTICS.trackEvent('on-screen', bucket);
+    });
+    pymChild.onMessage('scroll-depth', function(data) {
+        data = JSON.parse(data);
+        ANALYTICS.trackEvent('scroll-depth', data.percent, data.seconds);
+    });
+}
+
+/*
+ * Format graphic data for processing by D3.
+ */
+var formatData = function() {
+    console.log(DATA);
+    DATA.forEach(function(d) {
+        d['Black'] = +d['Black'];
+        d['White'] = +d['White'];
+        d['Hispanic'] = +d['Hispanic'];
+    });
+}
+
+/*
+ * Render the graphic(s). Called by pym with the container width.
+ */
+var render = function(containerWidth) {
+    if (!containerWidth) {
+        containerWidth = DEFAULT_WIDTH;
+    }
+
+    if (containerWidth <= MOBILE_THRESHOLD) {
+        isMobile = true;
+    } else {
+        isMobile = false;
+    }
+
+    // Render the chart!
+    renderColumnChart({
+        container: '#chart-black',
+        width: containerWidth,
+        data: DATA,
+        valueColumn: "Black"
+    });
+
+    renderColumnChart({
+        container: '#chart-hispanic',
+        width: containerWidth,
+        data: DATA,
+        valueColumn: "Hispanic"
+    });
+
+    renderColumnChart({
+        container: '#chart-white',
+        width: containerWidth,
+        data: DATA,
+        valueColumn: "White"
+    });
+
+    // Update iframe
+    if (pymChild) {
+        pymChild.sendHeight();
+    }
+}
+
+/*
+ * Render a column chart.
+ */
+var renderColumnChart = function(config) {
+    /*
+     * Setup chart container.
+     */
+    var labelColumn = 'label';
+    var valueColumn = config['valueColumn'];
+
+    var aspectWidth = isMobile ? 2 : 4;
+    var aspectHeight = 1;
+    var valueGap = 6;
+
+    var margins = {
+        top: 10,
+        right: 5,
+        bottom: isMobile ? 55 : 40,
+        left: 40
+    };
+
+    var roundTicksFactor = 50;
+
+    // Calculate actual chart dimensions
+    var chartWidth = config['width'] - margins['left'] - margins['right'];
+    var chartHeight = Math.ceil((config['width'] * aspectHeight) / aspectWidth) - margins['top'] - margins['bottom'];
+
+    // Clear existing graphic (for redraw)
+    var containerElement = d3.select(config['container']);
+    containerElement.html('');
+
+    /*
+     * Create the root SVG element.
+     */
+    var chartWrapper = containerElement.append('div')
+        .attr('class', 'graphic-wrapper');
+
+    var chartElement = chartWrapper.append('svg')
+        .attr('width', chartWidth + margins['left'] + margins['right'])
+        .attr('height', chartHeight + margins['top'] + margins['bottom'])
+        .append('g')
+        .attr('transform', 'translate(' + margins['left'] + ',' + margins['top'] + ')');
+
+    /*
+     * Create D3 scale objects.
+     */
+    var xScale = d3.scale.ordinal()
+        .rangeRoundBands([0, chartWidth], .1)
+        .domain(config['data'].map(function (d) {
+            return d[labelColumn];
+        }));
+
+    var yScale = d3.scale.linear()
+        .domain([0, 0.75])
+        .range([chartHeight, 0]);
+
+    /*
+     * Create D3 axes.
+     */
+    var xAxis = d3.svg.axis()
+        .scale(xScale)
+        .orient('bottom')
+        .tickFormat(function(d, i) {
+            return d;
+        });
+
+    var yAxis = d3.svg.axis()
+        .scale(yScale)
+        .orient('left')
+        .tickValues([0, 0.25, 0.5, 0.75])
+        .tickFormat(function(d) {
+            return (d * 100) + '%';
+        });
+
+    /*
+     * Render axes to chart.
+     */
+    chartElement.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', makeTranslate(0, chartHeight))
+        .call(xAxis)
+        .selectAll(".tick text")
+        .call(wrap, xScale.rangeBand());;
+
+    chartElement.append('g')
+        .attr('class', 'y axis')
+        .call(yAxis)
+
+    /*
+     * Render grid to chart.
+     */
+    var yAxisGrid = function() {
+        return yAxis;
+    };
+
+    chartElement.append('g')
+        .attr('class', 'y grid')
+        .call(yAxisGrid()
+            .tickSize(-chartWidth, 0)
+            .tickFormat('')
+        );
+
+    /*
+     * Render bars to chart.
+     */
+    chartElement.append('g')
+        .attr('class', 'bars')
+        .selectAll('rect')
+        .data(config['data'])
+        .enter()
+        .append('rect')
+            .attr('x', function(d) {
+                return xScale(d[labelColumn]);
+            })
+            .attr('y', function(d) {
+                if (d[valueColumn] < 0) {
+                    return yScale(0);
+                }
+
+                return yScale(d[valueColumn]);
+            })
+            .attr('width', xScale.rangeBand())
+            .attr('height', function(d) {
+                if (d[valueColumn] < 0) {
+                    return yScale(d[valueColumn]) - yScale(0);
+                }
+
+                return yScale(0) - yScale(d[valueColumn]);
+            })
+            .attr('class', function(d) {
+                return 'bar bar-' + d[labelColumn];
+            });
+
+    /*
+     * Render bar values.
+     */
+    chartElement.append('g')
+        .attr('class', 'value')
+        .selectAll('text')
+        .data(config['data'])
+        .enter()
+        .append('text')
+            .text(function(d) {
+                return (d[valueColumn] * 100).toFixed(1) + '%';
+            })
+            .attr('x', function(d, i) {
+                return xScale(d[labelColumn]) + (xScale.rangeBand() / 2);
+            })
+            .attr('y', function(d) {
+                return yScale(d[valueColumn]);
+            })
+            .attr('dy', function(d) {
+                var textHeight = d3.select(this).node().getBBox().height;
+                var barHeight = 0;
+
+                if (d[valueColumn] < 0) {
+                    barHeight = yScale(d[valueColumn]) - yScale(0);
+
+                    if (textHeight + valueGap * 2 < barHeight) {
+                        d3.select(this).classed('in', true);
+                        return -(textHeight - valueGap / 2);
+                    } else {
+                        d3.select(this).classed('out', true)
+                        return textHeight + valueGap;
+                    }
+                } else {
+                    barHeight = yScale(0) - yScale(d[valueColumn]);
+
+                    if (textHeight + valueGap * 2 < barHeight) {
+                        d3.select(this).classed('in', true)
+                        return textHeight + valueGap;
+                    } else {
+                        d3.select(this).classed('out', true)
+                        return -(textHeight - valueGap / 2);
+                    }
+                }
+            })
+            .attr('text-anchor', 'middle')
+}
+
+function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
+
+/*
+ * Initially load the graphic
+ * (NB: Use window.load to ensure all images have loaded)
+ */
+window.onload = onWindowLoaded;
