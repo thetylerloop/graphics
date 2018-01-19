b1b234c68f7506ac76058c69e1bbb46580e87e51
Christopher Groskopf <chrisgroskopf@gmail.com>
2018-01-18 19:25:48 -0600

    restaurants-per-capita: Initial import.

---
+++ restaurants-per-capita/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1Vy8T6pri_IZwNiPAJ_QjPdGRzwU5uFY5pxs7nYDujq0'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
