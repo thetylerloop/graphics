d20e9a35705fe8a2262d2a9497037bbd677ebd3f
Christopher Groskopf <chrisgroskopf@gmail.com>
2018-07-18 12:14:55 -0600

    airport-delays: initial import.

---
+++ airport-delays/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1DWc7oMhRZnic1AMFqIuB9DE8PyVjVdi3WnNAG8yo53Y'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
