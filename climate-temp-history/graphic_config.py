963e7f36be3c3b0bc5ec452d4598dfdfc6bbf7d4
Christopher Groskopf <chrisgroskopf@gmail.com>
2018-08-05 11:32:57 -0500

    Add climate-temp-history.

---
+++ climate-temp-history/graphic_config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import base_filters
+
+COPY_GOOGLE_DOC_KEY = '1Rbd5fG81_PB2C1P86K7SKYBrsdsA89lX7wSFKh2fJ-g'
+
+USE_ASSETS = False
+
+# Use these variables to override the default cache timeouts for this graphic
+# DEFAULT_MAX_AGE = 20
+# ASSETS_MAX_AGE = 300
+
+JINJA_FILTER_FUNCTIONS = base_filters.FILTERS
